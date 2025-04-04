4728471f749340017b3e45a8dc88fc7182381fc2
Tony Macintosh <Bradbeeschwarz@gmail.com>
2025-04-03 20:22:11 -0700

    Update Download.js

--- Download.js
+++ Download.js
@@ -1,4 +1,4 @@
 // Function to Download Repo ZIP
-        function downloadRepo() {
-            window.location.href = "https://github.com/Warp-Core-PW/Impact/raw/refs/heads/main/extensions.zip";
+        function Alt() {
+            window.location.href = "https://ZincCore.github.io/ZincShelf";
         }
